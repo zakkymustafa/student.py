bd7e8954b0b3224ca8a2203e3b80c946ca54b045
Rynah-Crynah <swabrinyusufshamun@gmail.com>
2022-03-21 21:43:33 +0300

    new changes

--- student.py
+++ student.py
@@ -1,9 +1,10 @@
 class Student:
 	"""docstring for Student"""
-	def __init__(self, first_name,second_name,age):
+	def __init__(self, first_name,second_name,age,country):
 		self.first_name = first_name
 		self.second_name = second_name
 		self.age = age
+		self.country = country
 	def full_name(self):
 		name= self.first_name + " "+ self.second_name
 		return name
@@ -15,7 +16,9 @@ class Student:
     def initials(self):
     	initial = self.first_name + self.second_name
     	return initial
-		
+
+	def countries(self):
+		return "Hello {}, you have visited {}".format(self.first_name,self.country))	
 
 
 
